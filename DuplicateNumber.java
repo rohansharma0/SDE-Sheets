186a21029bad1c75f57a6813f4497c1a3621f63c
rohansharma0 <iamrohan29@gmail.com>
2021-08-01 01:01:57 +0530

    D01-SortColors

--- DuplicateNumber.java
+++ DuplicateNumber.java
@@ -1,5 +1,38 @@
 public class DuplicateNumber {
-    public static void main(String[] args) {
+    public int findDuplicate(int[] nums) {
 
+//        Time - O(n) --- Space - O(n)
+//
+//        int[] arr = new int[nums.length];
+//        int n = 0;
+//
+//        for(int i = 0 ; i< nums.length ; i++){
+//            if(arr[nums[i]] != 0){
+//                n = nums[i];
+//                break;
+//            }
+//            arr[nums[i]]++;
+//        }
+//
+//        return n;
+
+//        Time - O(n) --- Space - O(1)
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        fast = nums[0];
+
+        while(fast != slow){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+
+        return slow;
     }
 }
