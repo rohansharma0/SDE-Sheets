fb0acc6b48875a532f95c3dcc783a90e39119e11
rohansharma0 <iamrohan29@gmail.com>
2021-08-01 00:39:26 +0530

    D01-SortColors

--- SortColors.java
+++ SortColors.java
@@ -1,5 +1,27 @@
 public class SortColors {
-    public static void main(String[] args) {
-        System.out.println("Rohan");
+    public void sortColors(int[] nums) {
+        int i = 0 ;
+        int j = 0;
+        int k = nums.length -1;
+
+        while(i <= k){
+            if(nums[i] == 0){
+                swap(nums , i,j);
+                i++;
+                j++;
+            }else if(nums[i] == 1){
+                i++;
+            }else if(nums[i]==2){
+                swap(nums , i , k);
+                k--;
+            }
+        }
+
+    }
+
+    public static void swap(int[] arr, int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
