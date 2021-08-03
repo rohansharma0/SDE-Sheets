dfd42203caa69759b42d41d2a87b889320c1cb03
rohansharma0 <iamrohan29@gmail.com>
2021-08-03 18:25:33 +0530

    D04-Rotate Image by 90deg

---
+++ RotateImage.java
@@ -0,0 +1,37 @@
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+
+        transpose(matrix);
+        reverse(matrix);
+
+    }
+
+    public static void transpose(int[][] mat){
+
+        for(int i = 0 ; i<mat.length ; i++){
+            for(int j = i; j < mat[0].length ; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+    }
+
+    public static void reverse(int[][] mat){
+
+        for(int i = 0 ; i< mat.length ; i++){
+
+            int li = 0 ;
+            int hi = mat[0].length -1;
+
+            while(li < hi){
+                int temp = mat[i][li];
+                mat[i][li] = mat[i][hi];
+                mat[i][hi] = temp;
+                li++;
+                hi--;
+            }
+        }
+
+    }
+}
